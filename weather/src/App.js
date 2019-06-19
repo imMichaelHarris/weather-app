dfc53334e179f6e33fb895ad18bbdc95c93ce65a
Michael Harris <mhguitar1997@gmail.com>
2019-06-19 15:54:36 -0700

    Added Weather view in app.js

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import WeatherViewContainer from "./views/WeatherViewContainer";
 
 function App() {
   return (
     <div className="App">
       <h1>Rory's Weather App</h1>
+      <WeatherViewContainer />
     </div>
   );
 }
--- weather/src/views/WeatherViewContainer.js
+++ weather/src/views/WeatherViewContainer.js
@@ -1,17 +1,20 @@
 import React from "react";
 import { connect } from "react-redux";
-import {getWeather} from '../actions'
+import { getWeather } from "../actions";
 
 import WeatherAlert from "./WeatherAlert";
 
 class WeatherViewContainer {
-
-    componentDidMount(){
-        //soon TM
-    }
+  componentDidMount() {
+    //soon TM
+    getWeather()
+  }
   render() {
     return <div>{/* <WeatherAlert /> */}</div>;
   }
 }
 
-export default connect(null, {getWeather})(WeatherViewContainer);
+export default connect(
+  null,
+  { getWeather }
+)(WeatherViewContainer);
