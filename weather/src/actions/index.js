098e82dbdc0b73e4b5aa80ed4cc482fcb8457405
Michael Harris <mhguitar1997@gmail.com>
2019-06-19 15:44:42 -0700

    Axios request and dispatch based on the promise being resolved or rejected

--- weather/src/actions/index.js
+++ weather/src/actions/index.js
@@ -1,9 +1,19 @@
-import axios from 'axios'
+import axios from "axios";
 
-export const FETCH_WEATHER_START = "FETCH_WEATHER_START"
-export const FETCH_WEATHER_SUCCESS = "FETCH_WEATHER_SUCCESS"
-export const FETCH_WEATHER_FAILED = "FETCH_WEATHER_FAILED"
+export const FETCH_WEATHER_START = "FETCH_WEATHER_START";
+export const FETCH_WEATHER_SUCCESS = "FETCH_WEATHER_SUCCESS";
+export const FETCH_WEATHER_FAILED = "FETCH_WEATHER_FAILED";
 
 export const getWeather = () => dispatch => {
-    dispatch({type: FETCH_WEATHER_START})
-}
+  dispatch({ type: FETCH_WEATHER_START });
+  axios
+    .get('https://api.weather.gov/alerts')
+    .then(res => {
+      console.log(res);
+      dispatch({ type: FETCH_WEATHER_SUCCESS });
+    })
+    .catch(err => {
+      console.log(err);
+      dispatch({type: FETCH_WEATHER_FAILED})
+    });
+};
