70398309591bbdf22d6a0da3d1bd530f6c1df2d0
Michael Harris <mhguitar1997@gmail.com>
2019-06-19 15:39:13 -0700

    getWeather action creator start

--- weather/src/actions/index.js
+++ weather/src/actions/index.js
@@ -1,5 +1,9 @@
 import axios from 'axios'
 
-export const FETCH_WEATHER = "FETCH_WEATHER"
+export const FETCH_WEATHER_START = "FETCH_WEATHER_START"
 export const FETCH_WEATHER_SUCCESS = "FETCH_WEATHER_SUCCESS"
 export const FETCH_WEATHER_FAILED = "FETCH_WEATHER_FAILED"
+
+export const getWeather = () => dispatch => {
+    dispatch({type: FETCH_WEATHER_START})
+}
