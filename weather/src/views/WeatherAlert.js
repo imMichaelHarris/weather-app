d61d843117347d19a5eea88d8c4c571c2cfcdfd1
Michael Harris <mhguitar1997@gmail.com>
2019-06-19 15:29:27 -0700

    Made WeatherAlert component

---
+++ weather/src/views/WeatherAlert.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const WeatherAlert = () => {
+    return <h1>Hey</h1>
+}
+
+export default WeatherAlert;
--- weather/src/views/WeatherViewContainer.js
+++ weather/src/views/WeatherViewContainer.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { connect } from 
+import { connect } from 'react-redux';
+
+import WeatherAlert from './WeatherAlert'
 
 const WeatherViewContainer = () => {
     return (
         <div>
-            
+            {/* <WeatherAlert /> */}
         </div>
     );
 };
